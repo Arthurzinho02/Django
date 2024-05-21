9e12e8b984c2b91e1c502811345e5d4b05d67dc1
Arthurzinho02 <arthur.matheus0607@gmail.com>
2024-05-21 12:57:22 -0300

    update

--- mysite/hotel/urls.py
+++ mysite/hotel/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.homepage, name="home"),
     path('quarto', views.quartos, name="quarto"),
-    path('reservar', views.nomeForm, name="reservar")
+    path('reservar', views.nomeForm, name="reservar"),
+    path('cadastro', views.cadastro, name="cadastro"),
+    path('login', views.login, name="login")
 ]
--- mysite/hotel/views.py
+++ mysite/hotel/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from .models import hotel, Quarto, user
-from .forms import NameForm
+from .forms import NameForm, FormCadastro, FormLogin
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login as auth_login 
 
 # Create your views here.
 def homepage(request):
@@ -10,7 +12,10 @@ def homepage(request):
     dados_hotel = hotel.objects.all()
     #adicionamos o objeto ao dicionario.
     context["dados_hotel"] = dados_hotel
-    return render(request, 'homepage.html', context)
+    if request.user.is_authenticated:
+        return render(request, 'homepage.html', context)
+    else:
+        return render(request, 'homepage2.html', context)
 
 def quartos(request):
     context = {}
@@ -44,3 +49,57 @@ def nomeForm(request):
         form = NameForm()
         context['form'] = form
         return render(request, 'reservar.html', context )
+    
+
+def cadastro(request):
+    context = {}
+    dados_quarto = Quarto.objects.all()
+    context['dados_quarto'] = dados_quarto
+    dados_hotel = hotel.objects.all()
+    context['dados_hotel'] = dados_hotel
+    if request.method == "POST":
+        form = FormCadastro (request.POST)
+        if form.is_valid():
+            var_first_name = form.cleaned_data['first_name']
+            var_last_name = form.cleaned_data['last_name']
+            var_user = form.cleaned_data['user']
+            var_email = form.cleaned_data['email']
+            var_password = form.cleaned_data['password']
+       
+            user = User.objects.create_user(username=var_user, email=var_email, password=var_password)
+            user.first_name = var_first_name
+            user.last_name = var_last_name
+            user.save()
+            return redirect('/')
+    else:
+        form = FormCadastro()
+        context['form'] = form
+        return render(request, 'cadastro.html', context )
+    
+def login(request):
+    context = {}
+    dados_quarto = Quarto.objects.all()
+    context['dados_quarto'] = dados_quarto
+    dados_hotel = hotel.objects.all()
+    context['dados_hotel'] = dados_hotel
+    if request.method == "POST":
+        form = FormLogin (request.POST)
+        if form.is_valid():
+            var_user = form.cleaned_data['user']
+            var_password = form.cleaned_data['password']
+
+            user = authenticate(username=var_user, password=var_password)
+            if user is not None:
+                return redirect('/')
+            else:
+                return HttpResponse("<h1>Usuário ou senha incorretos</h1>")
+    else:
+        form = FormLogin()
+        context['form'] = form
+        return render(request, 'login.html', context )
+
+# def reserva(request):
+#     if not request.user.is_authenticate:
+#         return render(request, "login.html", {'form', form})
+#     else:
+#         return HttpResponse("<h1>RESERVA DE QUARTO</h1>")
